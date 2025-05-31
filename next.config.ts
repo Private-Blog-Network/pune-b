f20e9aa8107ffefd4214631e4507494e3dab6797
Private-Blog-Network <117824108+Private-Blog-Network@users.noreply.github.com>
2025-05-31 13:24:59 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // Ignore TypeScript build errors in production
+    ignoreBuildErrors: true,
+  },
   eslint: {
     // This will ignore ESLint errors during builds
     ignoreDuringBuilds: true,
